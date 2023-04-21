3f3083ec84759f6f47bdfac45976bba41d72bea7
NitinSohal <nitin19sohal@gmail.com>
2023-04-21 12:59:56 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "88abfbd226d49f35ce1d9c0a23089845",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
